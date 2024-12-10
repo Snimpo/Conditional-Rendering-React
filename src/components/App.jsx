63a61a69aba99791bc7db0e40d2d52d0886cfddc
Silvio <nirvash533@gmail.com>
2024-12-10 16:54:36 -0500

    added the instructions of the exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@
 import React from "react";
 import Form from "./Form";
 
+var userIsRegistered = false;
+
 function App() {
   return (
     <div className="container">
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -8,3 +8,14 @@ createRoot(document.getElementById('root')).render(
       <App />
     </StrictMode>,
   )
+
+
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
+
+
+//Peronal notes: my approach would be creating 2 components one called Login and another one calles Register.
+//once that is done, I'd setup a ternary operator to render the proper component based on the value of userIsRegistered.
