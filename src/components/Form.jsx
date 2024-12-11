b7a9e1df9d5db3cffe76939ef101331d60593417
Silvio <nirvash533@gmail.com>
2024-12-11 11:19:40 -0500

    added the correct class name for the login form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,21 +5,20 @@ function Form() {
   return (
     <>
       <div>
-        {isLoggedIn ? 
-        
-        <form>
-          <input type="text" placeholder="Username"/>
-          <input type="password" placeholder="Password"/>
-          <button type="submit">Login</button>
-        </form>
-        :        
-        <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
-        <button type="submit">Register</button>
-        </form>
-      }
+        {isLoggedIn ? (
+          <form className="form">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+          </form>
+        ) : (
+          <form className="form">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button type="submit">Register</button>
+          </form>
+        )}
       </div>
     </>
   );
